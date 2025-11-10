2b8c7e55548c834a7947b2796b02d48b75510e72
blinry <mail@blinry.org>
2025-11-10 15:44:14 +0100

    emacs: Pull out a "changetracker"

--- emacs-plugin/teamtype.el
+++ emacs-plugin/teamtype.el
@@ -1,28 +1,10 @@
-(defun pos-to-line-col (pos)
-  (save-excursion
-    (goto-char pos)
-    (cons (- (line-number-at-pos) 1) (- pos (line-beginning-position)))))
+(require 'jsonrpc)
 
 (defun line-col-to-pos (line col)
   (save-excursion
     (goto-char (point-min))
     (forward-line line)
     (+ (line-beginning-position) col)))
-
-(defvar my-last-change-old-text nil)
-
-; Note: beg and end are 1-indexed "character positions"! Not sure yet what exactly that means.
-(defun my-before-change (beg end)
-  (setq my-last-change-old-text (buffer-substring-no-properties beg end)))
-
-(defun my-after-change (beg end len)
-  (message "Replaced '%s' with '%s' at position %d (%s)"
-           my-last-change-old-text
-           (buffer-substring-no-properties beg end)
-           beg
-           (pos-to-line-col beg)
-           )
-  (replace-region 0 0 0 0 "!"))
 
 (defun replace-region (line1 col1 line2 col2 text)
   (let ((beg (line-col-to-pos line1 col1))
@@ -33,7 +15,56 @@
         (goto-char beg)
         (insert text)))))
 
-(add-hook 'before-change-functions #'my-before-change)
-(add-hook 'after-change-functions #'my-after-change)
+(defun find-root (file)
+  (locate-dominating-file file ".teamtype"))
+
+(defun file-opened ()
+  (message "File %s" buffer-file-name)
+  (message "Root: %s" (find-root buffer-file-name))
+  (track-changes (lambda (from to replacement)
+                   (message "Replaced from (%s) to (%s) with %s" from to replacement)
+                   ))
+  )
+
+(add-hook 'find-file-hook #'file-opened)
+
+;;;; CHANGETRACKER
+
+(defvar-local my-last-change-old-text nil)
+
+(defvar-local change-callback nil
+  "Callback to run on buffer changes in the current buffer.")
+
+(defun pos-to-line-col (pos)
+  (save-excursion
+    (goto-char pos)
+    (cons (- (line-number-at-pos) 1) (- pos (line-beginning-position)))))
+
+; Note: beg and end are 1-indexed "character positions"! Not sure yet what exactly that means.
+(defun my-before-change (beg end)
+  (setq my-last-change-old-text (buffer-substring-no-properties beg end)))
+
+(defun my-after-change (beg end len)
+  (message "after")
+  ; (message "Replaced '%s' with '%s' at position %d (%s)"
+  ;          my-last-change-old-text
+  ;          (buffer-substring-no-properties beg end)
+  ;          beg
+  ;          (pos-to-line-col beg)
+  ;          )
+  (funcall change-callback
+           (pos-to-line-col beg)
+           (pos-to-line-col end)
+           (buffer-substring-no-properties beg end))
+  ;(replace-region 0 0 0 0 "!")
+  )
+
+(defun track-changes (callback)
+  (message "tracking")
+  (setq-local change-callback callback)
+  (add-hook 'before-change-functions #'my-before-change nil t)
+  (add-hook 'after-change-functions #'my-after-change nil t))
+
+;;;;
 
 (provide 'teamtype)
