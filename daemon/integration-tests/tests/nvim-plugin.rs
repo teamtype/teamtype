ethersync/ethersync

refactor(daemon): pull out some types to an editor protocol module

daemon/integration-tests/tests/nvim-plugin.rs
@@ -5,9 +5,12 @@
 
 use ethersync_integration_tests::actors::*;
 
-use ethersync::types::{
-    factories::*, EditorProtocolMessageFromEditor, EditorProtocolMessageToEditor,
-    EditorProtocolObject, EditorTextDelta, EditorTextOp, JSONRPCFromEditor,
+use ethersync::{
+    editor_protocol::{
+        EditorProtocolMessageFromEditor, EditorProtocolMessageToEditor, EditorProtocolObject,
+        JSONRPCFromEditor,
+    },
+    types::{factories::*, EditorTextDelta, EditorTextOp},
 };
 
 use pretty_assertions::assert_eq;

teamtype/teamtype

daemon/integration-tests/tests/nvim-plugin.rs
@@ -5,9 +5,12 @@
 
 use teamtype_integration_tests::actors::*;
 
-use teamtype::types::{
-    factories::*, EditorProtocolMessageFromEditor, EditorProtocolMessageToEditor,
-    EditorProtocolObject, EditorTextDelta, EditorTextOp, JSONRPCFromEditor,
+use teamtype::{
+    editor_protocol::{
+        EditorProtocolMessageFromEditor, EditorProtocolMessageToEditor, EditorProtocolObject,
+        JSONRPCFromEditor,
+    },
+    types::{factories::*, EditorTextDelta, EditorTextOp},
 };
 
 use pretty_assertions::assert_eq;