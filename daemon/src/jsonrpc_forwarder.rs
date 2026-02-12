11be8ff7ea23b1df2ac66970961788eea8b91bc0
Moritz Neeb <nt4u@kpvn.de>
2026-02-12 18:18:47 +0100

    refactor(daemon): Extract connect_stream method from JSONRPCForwarder
    More preparations for multi-platform: The idea is that it will be
    possible to have a different JSONRPCForwarder trait implementation
    with e.g. NamedPipes (as proposed in PR #260).
    
    Co-authored-by: d0mmi <mail@dommi.dev>

--- daemon/src/jsonrpc_forwarder.rs
+++ daemon/src/jsonrpc_forwarder.rs
@@ -16,8 +16,9 @@
 use async_trait::async_trait;
 use futures::{SinkExt, StreamExt};
 use std::path::Path;
-use tokio::io::{BufReader, BufWriter};
+use tokio::io::{AsyncRead, AsyncWrite, BufReader, BufWriter};
 use tokio::net::UnixStream;
+use tokio::net::unix::{OwnedReadHalf, OwnedWriteHalf};
 use tokio_util::bytes::{Buf, BytesMut};
 use tokio_util::codec::{Decoder, Encoder, FramedRead, FramedWrite, LinesCodec};
 
@@ -26,21 +27,40 @@ pub const DEFAULT_SOCKET_NAME: &str = "socket";
 pub const CONFIG_DIR: &str = ".teamtype";
 
 #[async_trait(?Send)]
-pub trait JSONRPCForwarder {
+pub trait JSONRPCForwarder<
+    R: AsyncRead + Unpin + Send + 'static,
+    W: AsyncWrite + Unpin + Send + 'static,
+>
+{
     async fn connection(&self, directory: &Path) -> anyhow::Result<()>;
+    async fn connect_stream(
+        &self,
+        directory: &Path,
+    ) -> anyhow::Result<(FramedRead<R, LinesCodec>, FramedWrite<W, LinesCodec>)>;
 }
 
 pub struct UnixJSONRPCForwarder {}
 
 #[async_trait(?Send)]
-impl JSONRPCForwarder for UnixJSONRPCForwarder {
-    async fn connection(&self, base_dir: &Path) -> anyhow::Result<()> {
+impl JSONRPCForwarder<OwnedReadHalf, OwnedWriteHalf> for UnixJSONRPCForwarder {
+    async fn connect_stream(
+        &self,
+        directory: &Path,
+    ) -> anyhow::Result<(
+        FramedRead<OwnedReadHalf, LinesCodec>,
+        FramedWrite<OwnedWriteHalf, LinesCodec>,
+    )> {
         // Construct socket object, which send/receive newline-delimited messages.
-        let socket_path = base_dir.join(CONFIG_DIR).join(DEFAULT_SOCKET_NAME);
+        let socket_path = directory.join(CONFIG_DIR).join(DEFAULT_SOCKET_NAME);
         let stream = UnixStream::connect(socket_path).await?;
         let (socket_read, socket_write) = stream.into_split();
-        let mut socket_read = FramedRead::new(socket_read, LinesCodec::new());
-        let mut socket_write = FramedWrite::new(socket_write, LinesCodec::new());
+        let socket_read = FramedRead::new(socket_read, LinesCodec::new());
+        let socket_write = FramedWrite::new(socket_write, LinesCodec::new());
+        Ok((socket_read, socket_write))
+    }
+
+    async fn connection(&self, base_dir: &Path) -> anyhow::Result<()> {
+        let (mut socket_read, mut socket_write) = self.connect_stream(base_dir).await?;
 
         // Construct stdin/stdout objects, which send/receive messages with a Content-Length header.
         let mut stdin = FramedRead::new(BufReader::new(tokio::io::stdin()), ContentLengthCodec);
