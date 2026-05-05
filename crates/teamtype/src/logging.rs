b9cb30020b6bf1cc8ff6ce2e4b812ef694367a88
Caleb Maclennan <caleb@alerque.com>
2026-05-05 23:09:37 +0300

    feat(daemon): Colorize messages from tracing by level but without labels

--- crates/teamtype/src/logging.rs
+++ crates/teamtype/src/logging.rs
@@ -5,11 +5,19 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use std::env;
+use std::fmt;
 
 use anyhow::Result;
+use nu_ansi_term::{Color, Style};
 use time::macros::format_description;
 use tracing::level_filters::LevelFilter;
 use tracing::subscriber;
+use tracing::{Event, Level, Subscriber};
+use tracing_subscriber::fmt::{
+    FmtContext,
+    format::{FormatEvent, FormatFields, Writer},
+};
+use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::{EnvFilter, FmtSubscriber, fmt::time::UtcTime};
 
 pub fn initialize() -> Result<()> {
@@ -18,9 +26,8 @@ pub fn initialize() -> Result<()> {
     if simplified_logging {
         let subscriber = FmtSubscriber::builder()
             .with_env_filter(EnvFilter::new("teamtype=info,fuzzer=info"))
-            .without_time()
-            .with_level(false)
-            .with_target(false)
+            .with_ansi(true) // Let the rest of the builder understand we are colorized.
+            .event_format(ColoredLine)
             .finish();
         subscriber::set_global_default(subscriber).expect("Setting default log subscriber failed");
     } else {
@@ -32,9 +39,59 @@ pub fn initialize() -> Result<()> {
             .with_env_filter(filter)
             .with_thread_ids(true)
             .with_timer(timer)
+            .with_level(true)
+            .pretty()
             .finish();
         subscriber::set_global_default(subscriber).expect("Setting default log subscriber failed");
     }
 
     Ok(())
+}
+
+/// Modified clone of `Compact` formatter from tracing-subscriber. Eschews the optional level
+/// labels, time formatting, and target options for hard coded defaults and uses the same ANSI
+/// handling internals to colorize the whole line instead of only the level label.
+struct ColoredLine;
+
+impl<S, N> FormatEvent<S, N> for ColoredLine
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+    N: for<'a> FormatFields<'a> + 'static,
+{
+    fn format_event(
+        &self,
+        ctx: &FmtContext<'_, S, N>,
+        mut writer: Writer<'_>,
+        event: &Event<'_>,
+    ) -> fmt::Result {
+        let mut buf = String::new();
+        let buf_writer = Writer::new(&mut buf);
+        // We told the event builder we're using ANSI escapes here, but we are about to wrap the
+        // output of the existing `Compact` formatter and we do *not* want it to colorize stuff
+        // for us. We'll do that by wrapping the whole buffered output.
+        let fmt = tracing_subscriber::fmt::format()
+            .compact()
+            .with_ansi(false)
+            .without_time()
+            .with_target(false)
+            .with_level(false);
+        fmt.format_event(ctx, buf_writer, event)?;
+        // We want the ANSI coloring to be inside the trailing newline of each message.
+        let line = buf.trim_end_matches('\n');
+        let style = if writer.has_ansi_escapes() {
+            match *event.metadata().level() {
+                Level::ERROR => Color::Red.normal(),
+                Level::WARN => Color::Yellow.normal(),
+                // The typical color for info logs would be `Green`, but that is overbearing when
+                // colorizing the whole line, so just render info messages in the terminal default.
+                Level::INFO => Style::default(),
+                Level::DEBUG => Color::Blue.normal(),
+                Level::TRACE => Color::Purple.normal(),
+            }
+        } else {
+            return writeln!(writer, "{line}");
+        };
+        write!(writer, "{}", style.paint(line))?;
+        writeln!(writer)
+    }
 }
