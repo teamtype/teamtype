09f1cecedceecb7c2dea4ee1a2289abe32b20bc1
Caleb Maclennan <caleb@alerque.com>
2026-05-27 17:49:07 +0300

    refactor(cli): Rename components to not be platform specific, ease Windows support

--- crates/teamtype/src/daemon.rs
+++ crates/teamtype/src/daemon.rs
@@ -33,7 +33,7 @@ use tracing::debug;
 use crate::config::has_git_remote;
 use crate::config::has_local_user_config;
 use crate::config::{BaseDir, Config, NetworkMode, Peer, VcsMode};
-use crate::config::{CONFIG_DIR, DEFAULT_SOCKET_NAME};
+use crate::config::{CONFIG_DIR, DEFAULT_LISTENER_NAME};
 use crate::document::{self, Document};
 use crate::editor::{self, EditorId, EditorWriter};
 use crate::editor_connection::EditorConnection;
@@ -1027,7 +1027,7 @@ impl DocumentActorHandle {
 #[must_use]
 pub struct Daemon {
     pub document_handle: DocumentActorHandle,
-    socket_path: PathBuf,
+    listener_path: PathBuf,
     config: Config,
     // We need to store the connection manager in order to keep the connection alive.
     connection_manager: peer::ConnectionManager,
@@ -1046,8 +1046,8 @@ impl Daemon {
         let document_handle = DocumentActorHandle::new(&config, ui, init, persist);
 
         // Start socket listener.
-        let socket_path = config.base_dir.join(CONFIG_DIR).join(DEFAULT_SOCKET_NAME);
-        editor::spawn_socket_listener(&socket_path, document_handle.clone(), ui)?;
+        let listener_path = config.base_dir.join(CONFIG_DIR).join(DEFAULT_LISTENER_NAME);
+        editor::spawn_listener(&listener_path, document_handle.clone(), ui)?;
 
         // Start file watcher.
         spawn_file_watcher(&config, document_handle.clone());
@@ -1084,7 +1084,7 @@ impl Daemon {
 
         Ok(Self {
             document_handle,
-            socket_path,
+            listener_path,
             config,
             connection_manager,
         })
@@ -1098,7 +1098,7 @@ impl Daemon {
 impl Drop for Daemon {
     fn drop(&mut self) {
         debug!("Daemon dropped, removing socket");
-        sandbox::remove_file(&self.config.base_dir, &self.socket_path)
+        sandbox::remove_file(&self.config.base_dir, &self.listener_path)
             .expect("Could not remove socket");
     }
 }
--- crates/teamtype/src/editor.rs
+++ crates/teamtype/src/editor.rs
@@ -75,26 +75,26 @@ pub fn strip_current_dir(path: &Path) -> PathBuf {
 /// # Panics
 ///
 /// Will panic if we fail to listen on the socket, or if we fail to accept an incoming connection.
-pub fn spawn_socket_listener(
-    socket_path: &Path,
+pub fn spawn_listener(
+    listener_path: &Path,
     document_handle: DocumentActorHandle,
     ui: &UserInterface,
 ) -> Result<()> {
-    let parent_path = socket_path
+    let parent_path = listener_path
         .parent()
         .context("Invalid socket creation location")?;
     // Make sure the parent directory of the socket is only accessible by the current user.
     check_mode(parent_path, 0o77700u32)?;
 
-    // Using the sandbox method here is technically unnecessary,
-    // but we want to really run all path operations through the sandbox module.
+    // Using the sandbox method here is technically unnecessary, but we want to really run all path
+    // operations through the sandbox module.
     // TODO: Use correct directory as guard.
-    if sandbox::exists(Path::new("/"), Path::new(&socket_path))
+    if sandbox::exists(Path::new("/"), Path::new(&listener_path))
         .expect("Failed to check existence of path")
     {
         // If there's an existing socket, try to connect to it as a client. If that fails, we assume
         // there's no other daemon running and we can delete the socket.
-        if StdUnixStream::connect(strip_current_dir(socket_path)).is_ok() {
+        if StdUnixStream::connect(strip_current_dir(listener_path)).is_ok() {
             bail!(
                 "Detected an existing daemon running for this directory. Rejecting to start another one."
             );
@@ -102,7 +102,12 @@ pub fn spawn_socket_listener(
         ui.warn(
             "An existing socket was found for this directory, but since the daemon seems to be defunct it is being removed."
         );
-        sandbox::remove_file(Path::new("/"), socket_path).expect("Could not remove socket");
+        sandbox::remove_file(Path::new("/"), listener_path).unwrap_or_else(|_| {
+            panic!(
+                "Could not remove existing daemon's listener '{}'",
+                listener_path.display()
+            )
+        });
     }
 
     // The std library function used to create sockets requires a path shorter than SUN_LEN, but the
@@ -115,9 +120,9 @@ pub fn spawn_socket_listener(
     // library without changing the parent thread's location for keeps.
     let previous_cwd = env::current_dir()?;
     env::set_current_dir(parent_path)?;
-    let listener = UnixListener::bind(strip_current_dir(socket_path))?;
+    let listener = UnixListener::bind(strip_current_dir(listener_path))?;
     env::set_current_dir(previous_cwd)?;
-    debug!("Listening on UNIX socket: {}", socket_path.display());
+    debug!("Listening on UNIX socket: {}", listener_path.display());
 
     tokio::spawn({
         let ui = ui.clone();
