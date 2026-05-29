ede315c77b5acd18ed4b2d7e670b96ed35bb6c5a
Caleb Maclennan <caleb@alerque.com>
2026-05-29 10:58:04 +0300

    refactor(cli): Extract UI handling to separate module

---
+++ crates/teamtype/src/cli_ui.rs
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2026 Caleb Maclennan <caleb@alerque.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+use anyhow::{Context, Result};
+use dialoguer::Confirm;
+use teamtype::traits::Interactions;
+
+#[derive(Clone)]
+pub struct ConsoleInteractions {}
+
+impl Interactions for ConsoleInteractions {
+    fn confirm(&self, question: &str) -> Result<bool> {
+        Confirm::new()
+            .with_prompt(question)
+            .interact()
+            .context("Failed to read answer to y/n prompt")
+    }
+
+    fn inform(&self, message: &str) {
+        println!("{message}");
+    }
+}
