df1e37cc0c50d5a06a96f095c4d808c82ca93cca
Caleb Maclennan <caleb@alerque.com>
2026-06-02 00:47:58 +0300

    refactor(crates): Unpublish interfaces not needed outside of crate

--- crates/teamtype/src/types.rs
+++ crates/teamtype/src/types.rs
@@ -201,7 +201,7 @@ impl FileTextDelta {
     }
 }
 
-pub type CursorId = String;
+pub(crate) type CursorId = String;
 
 #[derive(Serialize, Deserialize, PartialEq, Eq, Clone, Debug)]
 pub struct CursorState {
