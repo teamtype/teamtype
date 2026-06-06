afc01212a19f1dcaf7eedeb1daca8c4ec8f3072c
Caleb Maclennan <caleb@alerque.com>
2026-06-07 01:35:18 +0300

    refactor(cli): Move directory initialization out of config parser
    This resolves all sorts of scope problems and leaves a few more for
    later. This kicked off with knowing that needing an async function to
    parse a local config was just wrong. That turned out to be because it
    was attempting to resolve network peers inside the function that was
    supposed to parse the CLI arguments. That should definitely not need to
    be async, and that would be a problem for API consumers.
    
    Attempting to move that around, it became apparent that the entire
    system of pre-initializing some parts of the config, then interacting
    with the UI (confirming directory creation, etc.) then generating a new
    config, then finally passing that to actual functions was just off. It
    put a lot of things on the wrong side of bin/lib splits and nested in
    functions that had other jobs.
    
    Unraveling this meant making the config struct a static one-time
    initialization thing, then checking it and initialization the directory
    from that, then moving on to other steps.
    
    There are some awkward bits in here (it adds a bunch of repetitive
    expects) but those are all for cases we can a) prove internally won't
    happen because they will have errored out previously, and b) fixing
    those will involve adding correct error handling to a whole bunch of
    functions that have other unwraps in them too for no good reason.

--- crates/teamtype/src/setup.rs
+++ crates/teamtype/src/setup.rs
@@ -7,17 +7,24 @@ use std::path::{Path, PathBuf};
 use anyhow::bail;
 use anyhow::{Context, Result};
 use docstr::docstr;
+use git2::{Error as GitError, Repository};
 use microxdg::XdgApp;
 use tempfile::{TempDir, tempdir_in};
 
-use crate::config::{self};
+use crate::config::CONFIG_DIR;
+use crate::config::Config;
 use crate::sandbox;
 use crate::types::UserInterface;
 
+// Once we know what the base directory is going to be, either validate our access to it and
+// an existing config therein or setup a new config. In the event this step creates a temporary
+// directory we need to hang onto the handle as long as we're running.
 pub fn setup_teamtype_directory(
-    directory: Option<&Path>,
+    config: Config,
     ui: &UserInterface,
-) -> Result<(PathBuf, Option<TempDir>)> {
+) -> Result<(Config, Option<TempDir>)> {
+    let mut config = config;
+    let directory = config.base_dir;
     let (base_dir, temp_dir) = if let Some(directory) = directory.as_ref() {
         let base_dir = directory.canonicalize().with_context(|| {
             format!(
@@ -31,25 +38,25 @@ pub fn setup_teamtype_directory(
         (temp_dir.path().to_path_buf(), Some(temp_dir))
     };
     if !has_teamtype_directory(&base_dir) {
-        let teamtype_dir = base_dir.join(config::CONFIG_DIR);
+        let teamtype_dir = base_dir.join(CONFIG_DIR);
         if ui.confirm(&docstr!(format!
             /// '{}' hasn't been used as a Teamtype directory before.
             ///
-            /// Do you want to enable live collaboration here? (This will create a {}/ directory.)
+            /// Do you want to enable live collaboration here? (This will create a {CONFIG_DIR}/ directory.)
             base_dir.display(),
-            config::CONFIG_DIR
         ))? {
             sandbox::create_dir(&base_dir, &teamtype_dir)?;
             ui.log("Created! Resuming launch.");
         } else {
-            bail!("Aborting launch. Teamtype needs a .teamtype/ directory to function");
+            bail!("Aborting launch. Teamtype needs a {CONFIG_DIR}/ directory to function");
         }
     }
-    Ok((base_dir, temp_dir))
+    config.base_dir = Some(base_dir);
+    Ok((config, temp_dir))
 }
 
 fn has_teamtype_directory(dir: &Path) -> bool {
-    let teamtype_dir = dir.join(config::CONFIG_DIR);
+    let teamtype_dir = dir.join(CONFIG_DIR);
     // Using the sandbox method here is technically unnecessary,
     // but we want to really run all path operations through the sandbox module.
     sandbox::exists(dir, &teamtype_dir).expect("Failed to check") && teamtype_dir.is_dir()
@@ -78,4 +85,46 @@ fn get_app_cache_dir() -> Result<PathBuf> {
     // but we want to really run all path operations through the sandbox module.
     sandbox::create_dir(app_cache_dir_parent, &app_cache_dir)?;
     Ok(app_cache_dir)
+}
+
+pub(crate) fn ensure_teamtype_is_ignored(directory: &Path) -> Result<()> {
+    if teamtype_directory_should_be_ignored_but_isnt(directory) {
+        add_teamtype_to_local_gitignore(directory)?;
+    }
+    Ok(())
+}
+
+#[must_use]
+fn teamtype_directory_should_be_ignored_but_isnt(path: &Path) -> bool {
+    if let Ok(repo) = find_git_repo(path) {
+        let teamtype_dir = path.join(CONFIG_DIR);
+        return !repo
+            .is_path_ignored(teamtype_dir)
+            .expect("Should have been able to determine ignore state of path");
+    }
+    false
+}
+
+fn add_teamtype_to_local_gitignore(directory: &Path) -> Result<()> {
+    let mut ignore_file_path = directory.join(CONFIG_DIR);
+    ignore_file_path.push(".gitignore");
+
+    // It's very unlikely that .teamtype/.gitignore will already contain something, but let's
+    // still append.
+    let bytes_in = sandbox::read_file(directory, &ignore_file_path).unwrap_or_default();
+    // TODO: use String::from_utf8
+    let mut content = std::str::from_utf8(&bytes_in)?.to_string();
+
+    if !content.is_empty() && !content.ends_with('\n') {
+        content.push('\n');
+    }
+    content.push_str("/*\n");
+    let bytes_out = content.as_bytes();
+    sandbox::write_file(directory, &ignore_file_path, bytes_out)?;
+
+    Ok(())
+}
+
+pub(crate) fn find_git_repo(path: &Path) -> Result<Repository, GitError> {
+    Repository::discover(path)
 }
