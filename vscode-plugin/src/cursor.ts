f545ea9cb9d69953e27baaa17ad1afa0413d8a53
Mohamed El tamawey <tammwy22@gmail.com>
2026-06-29 13:04:31 +0300

    run: prettier and eslint

--- vscode-plugin/src/cursor.ts
+++ vscode-plugin/src/cursor.ts
@@ -3,85 +3,93 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import * as vscode from "vscode"
+import * as vscode from 'vscode';
 
 const selectionDecorationType = vscode.window.createTextEditorDecorationType({
-    backgroundColor: "#1a4978",
-    borderRadius: "0.1rem",
+    backgroundColor: '#1a4978',
+    borderRadius: '0.1rem',
     rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
     before: {
-        color: "#548abf",
-        contentText: "ᛙ",
-        margin: "0px 0px 0px -0.35ch",
-        textDecoration: "font-weight: bold; position: absolute; top: 0; font-size: 200%; z-index: 0;",
+        color: '#548abf',
+        contentText: 'ᛙ',
+        margin: '0px 0px 0px -0.35ch',
+        textDecoration:
+            'font-weight: bold; position: absolute; top: 0; font-size: 200%; z-index: 0;',
     },
-})
+});
 
 interface RemoteCursor {
-    name: string
-    uri: vscode.Uri
-    selection: vscode.DecorationOptions
+    name: string;
+    uri: vscode.Uri;
+    selection: vscode.DecorationOptions;
 }
 
-let cursors: Map<string, RemoteCursor[]> = new Map()
+let cursors: Map<string, RemoteCursor[]> = new Map();
 
-export function setCursor(userid: string, name: string, uri: vscode.Uri, selections: vscode.DecorationOptions[]) {
-    let usersCursors = cursors.get(userid)
+export function setCursor(
+    userid: string,
+    name: string,
+    uri: vscode.Uri,
+    selections: vscode.DecorationOptions[]
+) {
+    let usersCursors = cursors.get(userid);
     if (usersCursors) {
         // Remove all decorations by this user.
         for (let cursor of usersCursors) {
             // TODO: Refactor this into drawCursors below?
             const editors = vscode.window.visibleTextEditors.filter(
-                (editor) => editor.document.uri.toString() === cursor.uri.toString(),
-            )
+                (editor) =>
+                    editor.document.uri.toString() === cursor.uri.toString()
+            );
             for (const editor of editors) {
-                editor.setDecorations(selectionDecorationType, [])
+                editor.setDecorations(selectionDecorationType, []);
             }
         }
     }
 
     let newCursors = selections.map((s) => {
-        return {name, selection: s, uri}
-    })
-    cursors.set(userid, newCursors)
+        return { name, selection: s, uri };
+    });
+    cursors.set(userid, newCursors);
 
     const editors = vscode.window.visibleTextEditors.filter(
-        (editor) => editor.document.uri.toString() === uri.toString(),
-    )
+        (editor) => editor.document.uri.toString() === uri.toString()
+    );
     for (let editor of editors) {
-        drawCursors(editor)
+        drawCursors(editor);
     }
 }
 
 export function getCursorInfo(): string {
-    if (cursors.size == 0) {
-        return "(No cursors.)"
+    if (cursors.size === 0) {
+        return '(No cursors.)';
     } else {
-        let message: string[] = []
+        let message: string[] = [];
         cursors.forEach((usersCursors, _userid) => {
             for (let cursor of usersCursors) {
-                let line1 = cursor.selection.range.start.line + 1
-                let line2 = cursor.selection.range.end.line + 1
+                let line1 = cursor.selection.range.start.line + 1;
+                let line2 = cursor.selection.range.end.line + 1;
                 if (line1 > line2) {
-                    ;[line1, line2] = [line2, line1]
+                    [line1, line2] = [line2, line1];
                 }
-                let position = line1 == line2 ? `${line1}` : `${line1}-${line2}`
+                let position =
+                    line1 === line2 ? `${line1}` : `${line1}-${line2}`;
 
                 // TODO: Trim URI to the relevant parts inside the project.
-                message.push(`${cursor.name} @ ${cursor.uri}:${position}`)
+                message.push(`${cursor.name} @ ${cursor.uri}:${position}`);
             }
-        })
-        return message.join("\n")
+        });
+        return message.join('\n');
     }
 }
 
 export function drawCursors(editor: vscode.TextEditor | undefined) {
     if (editor) {
-        let uri = editor.document.uri
+        let uri = editor.document.uri;
         let allSelections = Array.from(cursors.values())
             .flat()
             .filter((cursor) => cursor.uri.toString() === uri.toString())
-            .map((cursor) => cursor.selection)
-        editor.setDecorations(selectionDecorationType, allSelections)
+            .map((cursor) => cursor.selection);
+        editor.setDecorations(selectionDecorationType, allSelections);
     }
 }
