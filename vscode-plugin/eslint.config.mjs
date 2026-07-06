b98a6a7a2e9b1bf3af995559212e674a53807fa8
Mohamed El tamawey <tammwy22@gmail.com>
2026-07-06 11:55:27 +0200

    chore(vscode): Switch from .eslintrc.json to eslint.config.mjs
    The former seems deprecated.

---
+++ vscode-plugin/eslint.config.mjs
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: NONE
+//
+// SPDX-License-Identifier: CC0-1.0
+
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default [
+  {
+    files: ['**/*.ts'],
+  },
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
+
+      curly: 'warn',
+      eqeqeq: 'warn',
+      'no-throw-literal': 'warn',
+      semi: 'off',
+    },
+  },
+  eslintConfigPrettier,
+];
