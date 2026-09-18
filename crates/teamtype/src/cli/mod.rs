d73a50905e745269626e26aacbd60e74cf7c3946
Caleb Maclennan <caleb@alerque.com>
2026-09-18 11:59:19 +0300

    refactor(cli): Cleanup file organization inside bin crate

---
+++ crates/teamtype/src/cli/mod.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2026 Caleb Maclennan <caleb@alerque.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pub mod args;
+pub mod config;
+pub mod logging;
+pub mod ui;
